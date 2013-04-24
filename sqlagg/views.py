2376420b6e6ed45814e492b3dc219cfefe742329
Simon Kelly <skelly@dimagi.com>
2013-04-24 17:56:45 +0200

    refactor custom query views

--- sqlagg/views.py
+++ sqlagg/views.py
@@ -20,6 +20,21 @@ class BaseColumnView(object):
         return self.table_name, str(self.filters), str(self.group_by)
 
 
+class CustomQueryView(BaseColumnView, QueryView):
+    query_cls = None
+    name = None
+
+    def get_query_meta(self, default_table_name, default_filters, default_group_by):
+        table_name = self.table_name or default_table_name
+        filters = self.filters or default_filters
+        group_by = self.group_by or default_group_by
+        return self.query_cls(table_name, filters, group_by)
+
+    @property
+    def view_key(self):
+        return self.name, self.key, self.table_name, str(self.filters), str(self.group_by)
+
+
 class SimpleView(BaseColumnView):
     pass
 
@@ -48,20 +63,6 @@ class UniqueView(BaseColumnView):
     aggregate_fn = lambda view, column: func.count(distinct(column))
 
 
-class MedianView(QueryView):
-    def __init__(self, key, as_name=None, table_name=None, filters=None, group_by=None):
-        self.key = key
-        self.as_name = as_name
-        self.table_name = table_name
-        self.filters = filters
-        self.group_by = group_by
-
-    def get_query_meta(self, default_table_name, default_filters, default_group_by):
-        table_name = self.table_name or default_table_name
-        filters = self.filters or default_filters
-        group_by = self.group_by or default_group_by
-        return MedianQueryMeta(table_name, filters, group_by)
-
-    @property
-    def view_key(self):
-        return "median", self.key, self.table_name, str(self.filters), str(self.group_by)
+class MedianView(CustomQueryView):
+    query_cls = MedianQueryMeta
+    name = "median"
