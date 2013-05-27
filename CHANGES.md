5e18625b55810bdcf997e8d5d8cb08319aad6827
Simon Kelly <skelly@dimagi.com>
2013-05-27 09:02:51 +0200

    bump version, add change history

---
+++ CHANGES.md
@@ -0,0 +1,6 @@
+History
+=========
+
+0.1.1 (2013-05-24)
+----------------
+* Added conditional columns (SumWhen)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='sqlagg',
-    version='0.1.0',
+    version='0.1.1',
     description='SQL aggregation tool',
     author='Dimagi',
     author_email='dev@dimagi.com',
