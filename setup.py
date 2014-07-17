525f5d6a4f0b0a876c1d1d9de97b0e448eae5e92
Simon Kelly <skelly@dimagi.com>
2014-07-17 10:31:04 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='sqlagg',
-    version='0.2.0',
+    version='0.2.1',
     description='SQL aggregation tool',
     author='Dimagi',
     author_email='dev@dimagi.com',
