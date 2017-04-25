5c90b2352fa49ba2110f40dfcc772bbecf247045
Simon Kelly <skelly@dimagi.com>
2017-04-25 09:59:10 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='sqlagg',
-    version='0.7.0',
+    version='0.8.0',
     description='SQL aggregation tool',
     author='Dimagi',
     author_email='dev@dimagi.com',
