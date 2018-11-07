9331283fcac9f913e73608c9e5340fc1df355559
Simon Kelly <skelly@dimagi.com>
2018-11-07 10:27:51 +0200

    bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require=[
 
 setup(
     name='sqlagg',
-    version='0.11.0',
+    version='0.11.1',
     description='SQL aggregation tool',
     author='Dimagi',
     author_email='dev@dimagi.com',
