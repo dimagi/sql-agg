a308990ba8a4eac1583d0c03c3421605ac6fed5b
Manish Kangia <mkangia@dimagi.com>
2020-03-26 16:29:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require=[
 
 setup(
     name='sqlagg',
-    version='0.16.0',
+    version='0.16.1',
     description='SQL aggregation tool',
     author='Dimagi',
     author_email='dev@dimagi.com',
