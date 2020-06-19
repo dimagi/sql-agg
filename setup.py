67982f633984c0ee943b8e725a0f8746c161b198
Simon Kelly <skelly@dimagi.com>
2020-06-19 16:26:54 +0200

    update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require=[
 
 setup(
     name='sqlagg',
-    version='0.16.1',
+    version='0.16.2',
     description='SQL aggregation tool',
     author='Dimagi',
     author_email='dev@dimagi.com',
